bc3cc4da73cdce3317dd9bd3c2ba1bc05a524305
rusticman <russellwheatley85@gmail.com>
2016-12-21 13:12:20 +0000

    typos fixed

--- src/containers/app.js
+++ src/containers/app.js
@@ -12,7 +12,7 @@ import Footer from '../components/footer';
     const options = {
       allowedConnections: ['twitter', 'facebook'],
       auth: {
-         redirectUrl: 'https://book-trade-server.herokuapp.com/allbooks',
+         redirectUrl: ' https://book-trade-rustic.herokuapp.com/allbooks',
          responseType: 'token'
     }
     }
